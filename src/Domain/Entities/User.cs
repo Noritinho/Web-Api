5dc411ce0739ca57060ae9c46b54076f5d1ea3e5
norita <marnori2001@gmail.com>
2024-07-26 22:16:11 -0300

    FEAT: add UserIdentifier(Guid) on User Entity

--- src/Domain/Entities/User.cs
+++ src/Domain/Entities/User.cs
@@ -1,10 +1,10 @@
 using Domain.Common;
-using Domain.Enums;
 
 namespace Domain.Entities;
 
 public class User : BaseAuditableEntity
 {
+    public Guid UserIdentifier { get; set; }
     public string Username { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
