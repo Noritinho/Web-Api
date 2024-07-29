b4d98aa3048b6fe261512b5f9f1d2a352677ba0e
norita <marnori2001@gmail.com>
2024-07-28 22:28:52 -0300

    FEAT: add authentication in program.cs

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,7 +1,10 @@
+using System.Text;
 using Api.Filters;
 using Application;
 using Infrastructure;
 using Infrastructure.Data.Migrations;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +19,22 @@ builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)))
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
+var signingKey = builder.Configuration.GetValue<string>("Settings:Jwt:SigningKey");
+builder.Services.AddAuthentication(config =>
+{
+    config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(config =>
+{
+    config.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ClockSkew = new TimeSpan(0),
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signingKey!))
+    };
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -27,6 +46,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
